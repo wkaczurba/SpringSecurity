c107aca52e00c3674bebaefd1e39d46eaedf6053
wkaczurba <witold.kaczurba@gmail.com>
2017-01-16 16:55:36 +0000

    Added thymeleaf + subpages.

--- SpringSecurity/src/main/java/com/some/web/WebConfig.java
+++ SpringSecurity/src/main/java/com/some/web/WebConfig.java
@@ -2,25 +2,65 @@ package com.some.web;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.view.JstlView;
+import org.thymeleaf.ITemplateEngine;
+import org.thymeleaf.spring4.SpringTemplateEngine;
+import org.thymeleaf.spring4.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring4.view.ThymeleafViewResolver;
+import org.thymeleaf.templateresolver.ITemplateResolver;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.ApplicationContext;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan("com.some.web")
-public class WebConfig extends WebMvcConfigurerAdapter {
-
-	@Bean
+public class WebConfig extends WebMvcConfigurerAdapter implements ApplicationContextAware {
+	
+	ApplicationContext applicationContext;
+	
+	public void setApplicationContext(ApplicationContext applicationContext) {
+		this.applicationContext = applicationContext;
+	}
+	
+/*	@Bean
 	public ViewResolver viewResolver() {
 		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
 		resolver.setViewClass(JstlView.class);
 		resolver.setPrefix("/WEB-INF/jsp/");
 		resolver.setSuffix(".jsp");
 		return resolver;
+	}*/
+	
+	@Bean
+	public ViewResolver viewResolver() {
+		ThymeleafViewResolver thymeLeafViewResolver = new ThymeleafViewResolver();
+		thymeLeafViewResolver.setTemplateEngine(templateEngine());
+		return thymeLeafViewResolver;
+	}
+	
+	@Bean
+	public ITemplateEngine templateEngine() {
+		SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+		templateEngine.setTemplateResolver(templateResolver());
+		return templateEngine;
+	}
+	
+	@Bean
+	public ITemplateResolver templateResolver() {
+		SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+		templateResolver.setApplicationContext(applicationContext);
+		templateResolver.setPrefix("/WEB-INF/views/");
+		templateResolver.setSuffix(".html");
+		templateResolver.setTemplateMode("HTML5");
+		
+		return templateResolver;
 	}
 }
 
